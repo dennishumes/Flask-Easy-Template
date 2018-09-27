8584d384f75281b8bf1915a02699d3fc569df3b0
Dennis Humes <33698532+dennishumes@users.noreply.github.com>
2018-09-27 14:56:54 -0500

    Update run.py

--- run.py
+++ run.py
@@ -13,13 +13,13 @@ enable_pretty_logging()
 
 PORT = 5000
 
-# ------- PRODUCTION CONFIG -------
-# http_server = HTTPServer(WSGIContainer(app))
-# http_server.bind(PORT)
-# http_server.start(0)
-# ioloop = tornado.ioloop.IOLoop().instance()
-# autoreload.start(ioloop)
-# ioloop.start()
+------- PRODUCTION CONFIG -------
+ http_server = HTTPServer(WSGIContainer(app))
+http_server.bind(PORT)
+ http_server.start(0)
+ ioloop = tornado.ioloop.IOLoop().instance()
+ autoreload.start(ioloop)
+ioloop.start()
 
 
 # ------- DEVELOPMENT CONFIG -------
